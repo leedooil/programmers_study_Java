fe95a6c3f7fdeaa3a76f7312ae0a7919a18167fb
leedooil <107297272+leedooil@users.noreply.github.com>
2022-12-20 19:36:34 +0900

    20221215

---
+++ day01/HelloWorld.java
@@ -0,0 +1,21 @@
+package day01;
+/**
+ * 
+ * @author �̵���
+ *
+ */
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+//		System.out.println("test");
+/*		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");	*/
+		System.out.println("주석 단축키 ctrl + /");
+	}
+
+}
---
+++ day01/HelloWorld_prac.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class HelloWorld_prac {
+
+	public static void main(String[] args) {
+		// 이 부분을 주석처리 해서 프로그램이 정상적으로 실행되도록 만들어 보세요.
+		System.out.println("HelloWorld");
+	}
+}
