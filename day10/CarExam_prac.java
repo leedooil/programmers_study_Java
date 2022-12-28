c651be4c17315bbbe16dda5a6a994f88de654a57
leedooil <107297272+leedooil@users.noreply.github.com>
2022-12-28 15:54:48 +0900

    20221228

---
+++ day10/CarExam_prac.java
@@ -0,0 +1,10 @@
+package day10;
+
+public class CarExam_prac {
+
+	public static void main(String[] args) {
+		
+		Car c1 = new Car();
+		
+	}
+}
---
+++ day10/ReferenceDataType.java
@@ -0,0 +1,8 @@
+package day10;
+
+public class ReferenceDataType {
+	int i = 4;
+	
+	String str= new String("Hello");
+	
+}
