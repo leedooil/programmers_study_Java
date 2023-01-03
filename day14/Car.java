1f4b6753f82906a598d3cd5d191602497c6bfc22
leedooil <107297272+leedooil@users.noreply.github.com>
2023-01-03 15:13:52 +0900

    20230103

---
+++ day14/Car.java
@@ -0,0 +1,13 @@
+package day14;
+
+public class Car {
+	
+	//타입 필드명
+	String name;
+	int number;
+	
+	public Car(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ day14/PersonExam.java
@@ -0,0 +1,12 @@
+package day14;
+
+public class PersonExam {
+	
+	public static void main(String[] args) {
+		
+		Person person = new Person("사람",25);
+		System.out.println(person.name);
+		System.out.println(person.age);
+	}
+	
+}
